34b88be5cf8b8c1c7fbe335590f81d80899fa2e4
Cristian <cristian.ms@hotmail.com>
2019-07-23 15:08:16 -0300

    Aplicando Locale nos pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ExemplosPipesComponent } from './components/exemplos-pipes/exemplos-pipes.component';
-import { CamelCasePipe } from './pipes/camel-case.pipe';
+import {AppComponent} from './app.component';
+import {ExemplosPipesComponent} from './components/exemplos-pipes/exemplos-pipes.component';
+import {CamelCasePipe} from './pipes/camel-case.pipe';
+
+import pt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+import {SettingsService} from './services/settings.service';
+registerLocaleData(pt, 'pt-BR');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent,
-    CamelCasePipe
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ExemplosPipesComponent,
+        CamelCasePipe
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        /*
+        {
+            provide: LOCALE_ID,
+            useValue: 'pt-BR'
+        }
+        */
+        SettingsService,
+        {
+            provide: LOCALE_ID,
+            deps: [SettingsService],
+            useFactory: (settingsService) => settingsService.getLocale()
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
